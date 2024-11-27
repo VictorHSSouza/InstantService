1efc40446664f3e8bf09754563b6ce8c67a4454d
ThiHS <thiagohermontsiqueira@gmail.com>
2024-11-27 11:42:45 -0300

    InstantService_1.3

---
+++ html/public/assets/js/mapa.js
@@ -0,0 +1,11 @@
+var map = L.map('map').setView([-19.914459057825173, -43.93065654559423], 13); // Coordenadas iniciais (São Paulo, como exemplo)
+
+// Adicionar camada de mapa do OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// Adicionar marcador (opcional)
+L.marker([-19.914459057825173, -43.93065654559423]).addTo(map)
+    .bindPopup('Você está aqui!')
+    .openPopup();
